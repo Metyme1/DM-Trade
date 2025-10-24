f7e120e79b5ade73fe930c2d7509c9ce007105b0
Metyme1 <millionmety@gmail.com>
2025-10-24 18:19:36 +0300

    nextconfig updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import Navbar from "@/components/pages/navbar";
 import HeroSection from "@/components/section/HeroSection";
-// import Footer from "@/components/pages/footer";
 
 export default function Home() {
   return (
     <main className="flex flex-col min-h-screen">
       <Navbar />
       <HeroSection />
-      {/* <Footer /> */}
     </main>
   );
 }
